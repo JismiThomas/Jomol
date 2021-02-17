711a871af71146eaf5de069341723359bcb2e979
JismiThomas <jismithomas@gmail.com>
2021-02-17 12:01:54 +0530

    welcome

---
+++ src/Flw1/com/org/Flower.java
@@ -0,0 +1,14 @@
+package Flw1.com.org;
+
+public abstract  class Flower {
+	public void homeloan() {
+		System.out.println("12");
+	}
+	public void eduloan() {
+		System.out.println("15");
+	
+	
+
+}
+
+}
---
+++ src/Flw1/com/org/Flower2.java
@@ -0,0 +1,19 @@
+package Flw1.com.org;
+
+public class Flower2 extends Flower {
+	 public void perloan() {
+		System.out.println("20");
+	 }
+	 public static void main(String[] args) {
+		
+	Flower2 Id = new Flower2();
+	Id.eduloan();
+	Id.homeloan();
+	Id.perloan();
+	
+	}
+
+	}
+
+
+
